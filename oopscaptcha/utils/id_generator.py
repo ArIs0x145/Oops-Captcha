df6381d8c0155425d200cee5424b31ebd8a44877
ArIs0x145 <aris0x145@gmail.com>
2025-04-03 20:32:05 +0800

    refactor: reduce if else

--- oopscaptcha/utils/id_generator.py
+++ oopscaptcha/utils/id_generator.py
@@ -8,12 +8,9 @@ class IDGenerator:
     
     @staticmethod
     def generate_captcha_id() -> str:
-        """Generate a unique captcha ID using current timestamp and UUID."""
-        if IDGenerator._fixed_timestamp:
-            timestamp_str = IDGenerator._fixed_timestamp
-        else:
-            timestamp_str = time.strftime("%Y%m%d_%H%M%S")
-            
+        
+        timestamp_str = IDGenerator._fixed_timestamp or time.strftime("%Y%m%d_%H%M%S")
+    
         # Use random if available, for better reproducibility with seed
         if random.getstate():
             uuid_hex = ''.join(f'{random.randint(0, 15):x}' for _ in range(12))
@@ -24,16 +21,8 @@ class IDGenerator:
     
     @staticmethod
     def set_fixed_timestamp(timestamp_str: str = None):
-        """Set a fixed timestamp for reproducibility.
-        
-        Args:
-            timestamp_str: A timestamp string, if None, will use current time.
-        """
-        if timestamp_str is None:
-            timestamp_str = time.strftime("%Y%m%d_%H%M%S")
-        IDGenerator._fixed_timestamp = timestamp_str
+        IDGenerator._fixed_timestamp = timestamp_str or time.strftime("%Y%m%d_%H%M%S")
         
     @staticmethod
     def reset_timestamp():
-        """Reset the fixed timestamp to None."""
         IDGenerator._fixed_timestamp = None 
